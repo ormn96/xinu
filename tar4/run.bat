656c90d3aa6886478386a5229698526f2f67701b
or man <Ormn1996@gmail.com>
2021-05-11 20:00:38 +0300

    tar4 prt3

--- tar4/run.bat
+++ tar4/run.bat
@@ -2,4 +2,4 @@ tcc -I.. -DTURBOC -c *.c
 tlib myXINU.lib -+ FREEMEM.obj -+ INITIALI.obj -+ GETMEM.obj -+ CLKINT.obj -+ KILL.obj 
 tlib myXINU.lib -+ freep.obj -+ insertdm.obj -+ memq.obj -+ memslp.obj
 tlib myXINU.lib -+ memwake.obj -+XFORK.obj -+ xforkSF.obj -+ xwait.obj
-tcc -I.. main.c myxinu.lib
+tcc -I.. test.c myxinu.lib
